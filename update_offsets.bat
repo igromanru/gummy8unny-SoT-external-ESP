43a76ca42f7537557b4d8b9eea8f8b8be01a00d2
igromanru <igromanru@yahoo.de>
2019-04-23 19:59:33 +0200

    switching to another Offsets-Updater

--- update_offsets.bat
+++ update_offsets.bat
@@ -1,2 +1,3 @@
 REM change the path to the SDK!
-node Offsets-Updater/offsets-updater.js ./Offsets.h D:\Git\Private\SDKs\SoT\SoT-SDK\SDK
+set CURRENT_PATH=%~dp0
+AutoIt.exe Offsets-Updater/UE-SDK-Offsets-Updater.au3 %CURRENT_PATH%Offsets.h D:\Git\Private\SDKs\SoT\SoT-SDK\SDK
